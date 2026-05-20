d5ffcf11074e6f6537f798109d77cbcb41d8cb8b
s-rayapudi <sandeep.rayapudi.in@gmail.com>
2026-05-20 12:52:38 +0530

    Added SignUp test in Authentication services

---
+++ src/test/java/com/api/tests/SignUpTest.java
@@ -0,0 +1,31 @@
+package com.api.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.api.base.AuthenticationServices;
+import com.api.models.request.SignUpRequest;
+
+import io.restassured.response.Response;
+
+public class SignUpTest {
+	
+	@Test(description = "Verify SignUp Test")
+	public void signupTest() {
+	
+		SignUpRequest signUpRequest = new SignUpRequest.Builder()
+				.userName("sandeep_sr2")
+				.password("Incorrect@123")
+				.email("sandeep@qe.com")
+				.firstName("Sandeep")
+				.lastName("Raya")
+				.mobileNumber("7848022338")
+				.build();
+
+		AuthenticationServices authServices = new AuthenticationServices();
+		Response response = authServices.signup(signUpRequest);
+		Assert.assertEquals(response.asPrettyString(), "User registered successfully!");
+		Assert.assertEquals(response.statusCode(), 200);
+	}
+	
+}
